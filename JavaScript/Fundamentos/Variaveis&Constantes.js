fb439bb499cb5d7ed11cd07ca3c62d5b73b4062c
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-08-20 18:51:51 -0300

    Create Variaveis&Constantes.js

---
+++ JavaScript/Fundamentos/Variaveis&Constantes.js
@@ -0,0 +1,18 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+
+// Aqui, usando constatens não podemos mudar. É diferente das variaveis;
+const c = 6
+// c = 70
+console.log(c)
