79a799222f33eb461147267d784e3aabf2483196
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-08-25 00:24:31 -0300

    Create 05.Object.js

---
+++ JavaScript/Fundamentos/05.Object.js
@@ -0,0 +1,22 @@
+// Objetos utiliza-se '{}' como formato.
+
+const prod1 = {}
+prod1.nome = 'Celular Ultra topster'
+prod1.preco = 8874.99
+prod1['Desconto legal'] = 0.40 // evitar usar espaço.
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 85.99
+}
+/*   obj:{
+        blabla: 1,
+        obj:{
+            blabla: 2
+        }
+    }
+*/
+
+console.log(prod2)
