0503b29592d6fdf6bfc5efd35a63148d09018411
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-09-02 19:25:41 -0300

    Create 18.Operadores_Unários.js

---
+++ JavaScript/Fundamentos/18.Operadores_Unários.js
@@ -0,0 +1,14 @@
+let num1= 1
+let num2= 2
+
+num1++
+console.log(num1)
+
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--) // não usar !!!
+
+
+// let exp = 2**3 (base ** expoente elevado)
+// console.log(exp) // 8
