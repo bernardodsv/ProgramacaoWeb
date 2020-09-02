42610245234d03fa0b0f0eafd417f7ce244eca76
Bernardo Viero <61853561+bernardodsv@users.noreply.github.com>
2020-09-02 17:55:51 -0300

    Create 15.Operadores_Aritmeticos.js

---
+++ JavaScript/Fundamentos/15.Operadores_Aritmeticos.js
@@ -0,0 +1,16 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a + b + c + d
+console.log(soma)
+
+const subtracao = d - b
+console.log(subtracao)
+
+const multiplicacao = a * b
+console.log(multiplicacao)
+
+const divisao = c / d 
+console.log(divisao)
+
+const modulo = a % 2
+console.log(modulo)
